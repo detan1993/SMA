7fd40068c542f6e498ccb18f19728eb9815119e6
shan <shanweiliew93@gmail.com>
2018-04-19 10:41:32 +0800

    Voucher profile and home layout

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Voucher } from '../../Entities/Voucher';
+import { VoucherProvider } from '../../providers/voucher/voucher';
 
 /**
  * Generated class for the ProfilePage page.
@@ -14,11 +16,33 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	vouchers : Voucher[];
+	
+  constructor(public navCtrl: NavController, public navParams: NavParams, public voucherProvider: VoucherProvider) {
   }
 
   ionViewDidLoad() {
+	  this.vouchers = [];
     console.log('ionViewDidLoad ProfilePage');
+  }
+  
+  ionViewDidEnter(){
+	  console.log("ionViewDidEnter()");
+	  this.getAllVouchers();
+  }
+  
+    getAllVouchers(){
+	  console.log("profile.ts getAllVouchers() called: " + parseInt(sessionStorage.getItem("userId")));
+	  this.voucherProvider.getAllVouchers(parseInt(sessionStorage.getItem("userId"))).subscribe(
+		 response =>{
+			  console.log("Response received");
+			  this.vouchers = response.voucherList
+			  console.log(this.vouchers);
+		  },
+		  error => {
+			  console.log("HTTP " + error.status + ": " + error.error.message);
+		  }
+	  );
   }
 
 }
